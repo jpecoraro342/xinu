1b9732552ce4e351d320d62bfe3f42567770a327
jpecoraro342 <jpecoraro342@ufl.edu>
2015-02-22 07:17:17 -0500

    Filled in syscalls - currently some bugs - writing test cases

--- system/main.c
+++ system/main.c
@@ -1,5 +1,34 @@
 /*  main.c  - main */
 
+/*
+* Lab 2 Checklist
+*
+* sendMSg:
+* 	√ Send Msg to Non-Full Process Queue (Test Case 1)
+*	? Sending Msg to Full Process Queue returns SYSERR
+*	? Sending Msg to bad pid returns SYSERR
+*
+* receiveMsg:
+* 	√ Receives message when a message is queued (Test Case 1)
+*	X Waits to receive message when none are queued (Test Case 2)
+*
+* sendMsgs:
+* 	? Sends multiple messages to a single process
+*	? Returns number of messages sent
+*	? Returns SYSERR on badpid
+* 
+* receiveMsgs:
+* 	? Receives number of messages if queued
+*	? Waits for number of messages to be queued before receiving
+*
+* sendnMsg:
+* 	√ Sends messages to all pids
+*	? Returns number successfully sent
+*	? Returns SYSERR on badpid
+*	? Does not send any messages if badpid encoutnered
+*
+*/
+
 #include <xinu.h>
 #include <stdio.h>
 
@@ -11,6 +40,8 @@ umsg32 message_queue[NPROC * MSG_BUFFER_SIZE]; /* The message queue for each pro
 int head[NPROC]; /* list of head pointers for each process */
 int tail[NPROC]; /* list of tail pointers for each process */
 
+sid32 print_mutex; /* Mutex Used for Printing */
+
 /* Message Queue Functions */
 int enqueue_msg(umsg32 msg, pid32 pid);
 umsg32 dequeue_msg(pid32 pid);
@@ -18,17 +49,23 @@ umsg32 peek_msg_queue(pid32 pid);
 int msg_queue_empty(pid32 pid);
 int msg_queue_full(pid32 pid);
 
+/* Printing Functions */
+void print_send_msg(pid32 pid, umsg32 msg);
+void print_receive_msg(umsg32 msg);
+
+/* Function for testing */
 void test();
 
-void print_proc_msg_info(pid32 pid);
-
 int main(int argc, char **argv) {
+	print_mutex = semcreate(1);
+
 	int i;
 	for (i = 0; i < NPROC; i++) {
 		head[i] = MSG_BUFFER_SIZE - 1;
 	}
 
-	resume(create(test, 4096, 50, "Start Teting", 0, 0));
+	test();
+	//resume(create(test, 4096, 50, "Start Testing", 0, 0));
 
 	while (1);
 	return OK;
@@ -37,29 +74,84 @@ int main(int argc, char **argv) {
 /* Testing Process */
 
 void test() {
-	intmask mask = disable();
-	pid32 testpid = 0;
+	
+	/* Test Case 1
+	 *
+	 * proc1 and proc2 send msg 1 and 2 to proc3
+	 * proc3 receives msg at front of queue
+	 *
+	 * Expected Output:
+	 * "Sent Msg 1"
+	 * "Sent Msg 2"
+	 * "Receive Msg 1"
+	 * 
+	 */
 
-	kprintf("head: %d tail: %d\n", head[testpid], tail[testpid]);
-	sleepms(500);
+	umsg32 msg1 = 1;
+	umsg32 msg2 = 2;
+	
+	// Receive Process
+	pid32 proc3 = create(receiveMsg, 4096, 50, "Receive", 0, 0);
 
-	while (!msg_queue_full(testpid)) {
-		enqueue_msg(5, testpid);
+	// Send Processes
+	pid32 proc1 = create(sendMsg, 4096, 50, "Send message to proc3", 2, proc3, msg1);
+	pid32 proc2 = create(sendMsg, 4096, 50, "Send message to proc3", 2, proc3, msg2);
+
+	resume(proc1);
+	resume(proc2);
+
+	resume(proc3);
+
+	/* End Test Case 1 */
+
+	/* Test Case 2
+	 *
+	 * proc4 sends msg3 to proc5
+	 * proc5 waits to receive message until sent by proc4
+	 *
+	 * Expected Output:
+	 * "Sent Msg 3"
+	 * "Receive Msg 3"
+	 * 
+	 */
+
+	umsg32 msg3 = 1;
+	
+	// Receive Process
+	pid32 proc5 = create(receiveMsg, 4096, 50, "Receive", 0, 0);
+
+	// Send Processes
+	pid32 proc4 = create(sendMsg, 4096, 50, "Send message to proc3", 2, proc5, msg3);
+
+	resume(proc5);
+	resume(proc4);
+
+	/* End Test Case 2 */
+
+	//umsg32 msg_list_1[5] = { 1, 2, 3, 4, 5 };
+	//umsg32 msg_list_1_rec[5];
+
+	/* receive 5 messages */
+	//pid32 proc1 = create(receiveMsgs, 4096, 50, "Receive 5 Messages", 2, &msg_list_1_rec, 5);
+
+	/* proc6 will send msg_list_1 to proc1 */
+	//pid32 proc6 = create(sendMsgs, 4096, 50, "Send 5 to proc1", 3, proc1, &msg_list_1, 5);
+
+	/*
+	//sender list processes
+	int i;
+	for (i = 0; i < 5; i++) {
+		sender_list[i] = create(receiveMsg, 4096, 50, "Receive", 0, 0);;
 	}
 
-	kprintf("head: %d tail: %d\n", head[testpid], tail[testpid]);
-	sleepms(500);
+	pid32 sender_list[5];
 
-	while (!msg_queue_empty(testpid)) {
-		umsg32 msg = dequeue_msg(testpid);
-		kprintf("item: %d msg: %d\n", head[testpid], msg);
-		sleepms(300);
+	pid32 proc11 = create(sendnMsg, 4096, 50, "Send 5 messages", 3, 5, &sender_list, msg22);
+
+	for (i = 0; i < 5; i++) {
+		resume(sender_list[i]);
 	}
-
-	kprintf("head: %d tail: %d\n", head[testpid], tail[testpid]);
-	sleepms(500);
-
-	resume(mask);
+	*/
 }
 
 /* Message Passing Functions */
@@ -70,7 +162,36 @@ void test() {
 * It will return OK on success or SYSERR on error.
 */
 syscall sendMsg (pid32 pid, umsg32 msg) {
+	intmask	mask = disable();
 
+	if (isbadpid(pid)) {
+		wait(print_mutex);
+		kprintf("Could not sent message to Process: %d - Bad PID\n", pid);
+		signal(print_mutex);
+
+		restore(mask);
+		return SYSERR;
+	}
+
+	//TODO: Change this if we need to be able to handle this
+	if (msg_queue_full(pid)) {
+		wait(print_mutex);
+		kprintf("Could not sent message to Process: %d - Message Queue Full\n", pid);
+		signal(print_mutex);
+
+		restore(mask);
+		return SYSERR;
+	}
+
+	enqueue_msg(msg, pid);
+
+	print_send_msg(pid, msg);
+	wait(print_mutex);
+	kprintf("Process: %d successfully sent 1 message\n", currpid);
+	signal(print_mutex);
+
+	restore(mask);
+	return OK;
 }
 
 /*
@@ -78,7 +199,20 @@ syscall sendMsg (pid32 pid, umsg32 msg) {
 * to be sent (by any process). When a message is sent, it is received and returned.
 */
 umsg32 receiveMsg (void) {
+	umsg32 msg;
+	if (!msg_queue_empty(currpid)) {
+		msg = dequeue_msg(currpid); 
+	}
+	else {
+		//Reschedule? Wait? Do something?
 
+		wait(print_mutex);
+		kprintf("Could not receive message on process: %d - Message Queue Empty\n", currpid);
+		signal(print_mutex);
+	}
+
+	print_receive_msg(msg);
+	return msg;	
 }
 
 /*
@@ -86,7 +220,35 @@ umsg32 receiveMsg (void) {
 * the number of msgs successfully sent or SYSERR on error
 */
 uint32 sendMsgs (pid32 pid, umsg32* msgs, uint32 msg_count) {
+	intmask	mask = disable();
 
+	if(isbadpid(pid)) {
+		wait(print_mutex);
+		kprintf("Could not sent messages to Process: %d - Bad PID\n", pid);
+		signal(print_mutex);
+
+		restore(mask);
+		return SYSERR;
+	}
+
+	uint32 successfull_messages = 0;
+	uint32 i;
+	for (i = 0; i < msg_count; i++) {
+		if (enqueue_msg(msgs[i], pid)) {
+			print_send_msg(pid, msgs[i]);
+			successfull_messages++;
+		}
+		else {
+			break;
+		}
+	}
+
+	wait(print_mutex);
+	kprintf("Process: %d Successfully Sent %d Messages\n", currpid, successfull_messages);
+	signal(print_mutex);
+
+	restore(mask);
+	return i;
 }
 
 /*
@@ -95,7 +257,17 @@ uint32 sendMsgs (pid32 pid, umsg32* msgs, uint32 msg_count) {
 * are then all together immediately received.
 */
 syscall receiveMsgs (umsg32* msgs, uint32 msg_count) {
+	//Currently it does not receive all at once
+	int i;
+	for (i = 0; i < msg_count; i++) {
+		msgs[i] = receive();
+	}
 
+	wait(print_mutex);
+	kprintf("Process: %d Successfully Recieved %d Messages\n", currpid, msg_count);
+	signal(print_mutex);	
+
+	return OK;
 }
 
 /*
@@ -103,7 +275,28 @@ syscall receiveMsgs (umsg32* msgs, uint32 msg_count) {
 * the number of pids successfully sent to or SYSERR on error.
 */
 uint32 sendnMsg (uint32 pid_count, pid32* pids, umsg32 msg) {
+	intmask	mask = disable();
 
+	uint32 successfull_messages = 0;
+	uint32 i;
+	for (i = 0; i < pid_count; i++) {
+		pid32 pid = pids[i];
+		if (isbadpid(pid)) {
+			continue;
+		}
+
+		if (enqueue_msg(msg, pid)) {
+			print_send_msg(pid, msg);
+			successfull_messages++;
+		}
+	}
+
+	wait(print_mutex);
+	kprintf("Process: %d Successfully Sent %d Messages\n", currpid, i);
+	signal(print_mutex);
+
+	restore(mask);
+	return i;
 }
 
 /* Circular Buffer Functions */
@@ -165,3 +358,16 @@ umsg32 dequeue_msg(pid32 pid) {
 
 /* Printing/Debugging */
 
+/* Prints: Sent Message: %d to PID: %d */
+void print_send_msg(pid32 pid, umsg32 msg) {
+	wait(print_mutex);
+	kprintf("Sent Message: %d to PID: %d\n", msg, pid);
+	signal(print_mutex);
+}
+
+/* Prints: Received Message: %d */
+void print_receive_msg(umsg32 msg) {
+	wait(print_mutex);
+	kprintf("Current Process: %d Received Message: %d\n", currpid, msg);
+	signal(print_mutex);
+}
